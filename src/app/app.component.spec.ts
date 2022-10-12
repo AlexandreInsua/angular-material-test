767aebc6931ce0452093907826a81ef4ebf1c5e2
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2022-10-12 22:52:32 +0200

    chore(project): set up project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'materials-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('materials-test');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('materials-test app is running!');
-  });
 });
---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { MatSliderModule } from "@angular/material/slider";
+
+@NgModule({
+    imports: [
+        MatSliderModule
+    ],
+    exports: [MatSliderModule]
+})
+export class MaterialModule {}
