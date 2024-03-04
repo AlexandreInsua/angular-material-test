8f87071e375478a227fae15061eab0320cabd6fb
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-03-04 21:15:56 +0100

    feature(project) adjust app to standalone app structure

---
+++ src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      BrowserModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      MatButtonModule
+    ),
+    provideAnimations(),
+  ],
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TableComponent } from './components/table/table.component';
-import { PrivateComponent } from './components/private/private.component';
 import { loginGuard } from './guards/login.guard';
+import { PrivateComponent } from './components/private/private.component';
+import { DragdropComponent } from './dragdrop/dragdrop.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    title: 'home',
   },
   {
     path: 'table',
     component: TableComponent,
+    title: 'table',
   },
   {
     path: 'private',
     canActivate: [loginGuard],
     component: PrivateComponent,
+    title: 'private',
+  },
+  {
+    path: 'drag-and-drop',
+    component: DragdropComponent,
+    title: 'drag and drop',
   },
   {
     path: '**',
     component: HomeComponent,
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
