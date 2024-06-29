ffe8cb506e699d9eb47e925433e072998af792f3
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-06-29 20:02:44 +0200

    chore(project): remove import

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from '../../services/user.service';
