38824806167071f0abf497a5c61ef742667c3ae6
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-08-21 17:24:41 +0200

    feat(IN-1): add drag functionality to todo card

--- src/app/components/kanban/components/todo/todo.component.ts
+++ src/app/components/kanban/components/todo/todo.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Todo } from '../../model/todo';
 import { MatCardModule } from '@angular/material/card';
 import { DatePipe } from '@angular/common';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [DatePipe, MatCardModule],
+  imports: [CdkDrag, DatePipe, MatCardModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
--- src/app/components/kanban/components/todos-wrapper/todos-wrapper.component.ts
+++ src/app/components/kanban/components/todos-wrapper/todos-wrapper.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TodoComponent } from '../todo/todo.component';
 import { Todo } from '../../model/todo';
+import { CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todos-wrapper',
   standalone: true,
-  imports: [TodoComponent],
+  imports: [CdkDropList, TodoComponent],
   templateUrl: './todos-wrapper.component.html',
   styleUrl: './todos-wrapper.component.scss',
 })
